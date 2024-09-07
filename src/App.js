2c266b20f937d2c38c1227e46b37a8ed5cee8e13
Muskan Swarup <muskanswarup680@gmail.com>
2024-09-07 15:33:49 +0530

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Let's get started with youtube</h1>
+    <div >
+     {/* <h1 >Let's get started with youtube</h1> */}
+     <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaSearch } from "react-icons/fa";
+import { RiVideoAddFill } from "react-icons/ri";
+import { IoIosNotifications } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import Avatar from 'react-avatar';
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-2 m-2 items-start">
+        {/* First div with hamburger menu and logo */}
+      <div className="flex items-center space-x-4">
+        <GiHamburgerMenu  size={20}/>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/3/34/YouTube_logo_%282017%29.png?20170829160812"
+          alt="youtube_logo"
+          width={"115px"}
+        ></img>
+      </div>
+      {/* Second div with search bar  */}
+      <div className="flex-1 flex justify-center">
+        <div className="flex items-center w-full max-w-lg">
+          <input
+            placeholder="Search"
+            className="w-full h-10 pl-4 border border-gray-400 outline-none rounded-l-full text-sm placeholder-gray-500"
+          />
+          <button className="w-16 h-10 flex justify-center items-center border border-gray-400 p-1 rounded-r-full">
+            <FaSearch  size={18}/>
+          </button>
+        </div>
+      </div>
+      {/* Third div with 3 icons */}
+      <div className="flex items-center space-x-4 ">
+          <button className="hover:bg-slate-400 rounded-lg">
+            <RiVideoAddFill size={20}/>
+          </button>
+          <button>
+            <IoIosNotifications  size={20}/>        
+          </button>
+          <button>
+            {/* <CgProfile  size={18}/> */}
+            <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxctjU21pUENIsGN1F4qY21P7GfdEbhTMp2g&s" size={30} round={true}/>
+          </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
