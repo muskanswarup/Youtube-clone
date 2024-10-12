a918ea835b9cd8f1d50f16b5a84a914d27f881ba
Muskan Swarup <muskanswarup680@gmail.com>
2024-10-12 16:55:59 +0530

    added yt constants

---
+++ src/constants/youtube.js
@@ -0,0 +1,3 @@
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const YOUTUBE_VIDEO_API =  process.env.REACT_APP_YOUTUBE_VIDEO_API;
